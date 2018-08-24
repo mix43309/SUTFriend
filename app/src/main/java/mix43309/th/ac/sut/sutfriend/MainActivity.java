30fc895db973b42cfdb07efad947ac1339cee06b
mix43309 <mix43309@gmail.com>
2018-08-24 10:54:29 +0700

    Initial commit

---
+++ app/src/main/java/mix43309/th/ac/sut/sutfriend/MainActivity.java
@@ -0,0 +1,13 @@
+package mix43309.th.ac.sut.sutfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
